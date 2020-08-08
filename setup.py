a092d4252f332769f0e0acaffe4c77fa20a00dce
alphabet5 <alphabet5@users.noreply.github.com>
2020-08-07 23:21:10 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/alphabet5/yamlarg",
     packages=setuptools.find_packages(),
+    classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
