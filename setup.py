f415b642d7b0ecd68e9532cf763937ad8a93450c
alphabet5 <alphabet5@users.noreply.github.com>
2020-08-07 23:36:13 -0500

    Update help for parse function.
    Update version info for pypi.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="yamlarg", # Replace with your own username
-    version="0.0.4",
+    version="0.0.6",
     author="John Burt",
     author_email="",
     description="Easy YAML arguments for python scripts.",
